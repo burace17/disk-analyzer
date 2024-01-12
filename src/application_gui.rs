b4be5f370cfd4cf40c2453300d72208adf6445c2
Albert Snow <ajsnow2012@gmail.com>
2024-01-11 23:48:17 -0600

    working on gui

--- src/application_gui.rs
+++ src/application_gui.rs
@@ -2,21 +2,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-// use gtk::{Window, Inhibit, WindowType};
-// use gtk::prelude::*;
-// use relm::{connect, Channel, Relm, Update, Widget, Component, init};
-// use relm_derive::Msg;
 // use std::thread;
 // use std::sync::{Arc, Mutex};
 // use std::sync::mpsc::{channel, Sender};
+use iced::widget::pick_list;
 // use super::dir_walker;
 // use super::analyzer;
-
+use iced::{Command, Application, Theme, Element, Length};
+use iced::executor;
 // pub struct ConfigModel {
 //     path: Option<std::path::PathBuf>,
 //     relm: Relm<ConfigWindow>
 // }
-
 // #[derive(Msg)]
 // pub enum ConfigMsg {
 //     Quit,
@@ -25,17 +22,6 @@
 //     GotResults(Arc<Mutex<dir_walker::Directory>>),
 //     CancelScan
 // }
-
-// pub struct ConfigWindow {
-//     model: ConfigModel,
-//     window: Window,
-//     file_chooser: gtk::FileChooserButton,
-//     scan_button: gtk::Button,
-//     analyzer_win: Option<Component<analyzer::AnalyzerWindow>>,
-//     cancel_sender: Option<Sender<()>>,
-//     cancel_button: gtk::Button
-// }
-
 // impl ConfigWindow {
 //     fn reset_ui(&self) {
 //         self.scan_button.set_label("Load");
@@ -43,29 +29,24 @@
 //         self.file_chooser.set_sensitive(true);
 //         self.cancel_button.set_sensitive(false);
 //     }
-
 //     fn on_scan_start(&mut self) {
 //         if let Some(file_path) = self.model.path.clone() {
 //             let stream = self.model.relm.stream().clone();
 //             let (_, sender) = Channel::new(move |dir| {
 //                 stream.emit(ConfigMsg::GotResults(dir));
 //             });
-
 //             let (send, recv) = channel();
 //             self.cancel_sender = Some(send);
-
 //             self.scan_button.set_label("Reading...");
 //             self.scan_button.set_sensitive(false);
 //             self.file_chooser.set_sensitive(false);
 //             self.cancel_button.set_sensitive(true);
-
 //             thread::spawn(move || {
 //                 let dir = dir_walker::read_dir(&file_path, &recv);
 //                 sender.send(dir).expect("Couldn't send message");
 //             });
 //         }
 //     }
-
 //     fn on_scan_complete(&mut self, dir: Arc<Mutex<dir_walker::Directory>>) {
 //         self.cancel_sender = None;
 //         let dir_clone = dir.clone();
@@ -90,7 +71,6 @@
 //             }
 //         }
 //     }
-
 //     fn on_scan_cancel(&self) {
 //         self.cancel_button.set_sensitive(false);
 //         if let Some(tracker) = &self.cancel_sender {
@@ -98,19 +78,16 @@
 //         }
 //     }
 // }
-
 // impl Update for ConfigWindow {
 //     type Model = ConfigModel;
 //     type ModelParam = ();
-//     type Msg = ConfigMsg;
-    
+//     type Msg = ConfigMsg;    
 //     fn model(relm: &Relm<Self>, _: ()) -> ConfigModel {
 //         ConfigModel {
 //             path: None,
 //             relm: relm.clone()
 //         }
 //     }
-
 //     fn update(&mut self, event: ConfigMsg) {
 //         match event {
 //             ConfigMsg::Quit => gtk::main_quit(),
@@ -121,14 +98,11 @@
 //         }
 //     }
 // }
-
 // impl Widget for ConfigWindow {
 //     type Root = Window;
-
 //     fn root(&self) -> Self::Root {
 //         self.window.clone()
 //     }
-
 //     fn view(relm: &Relm<Self>, model: Self::Model) -> Self {
 //         let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
 //         let file_chooser = gtk::FileChooserButton::new("Choose directory", gtk::FileChooserAction::SelectFolder);
@@ -137,24 +111,20 @@
 //         let cancel_button = gtk::Button::new();
 //         cancel_button.set_label("Cancel");
 //         cancel_button.set_sensitive(false);
-
 //         vbox.add(&file_chooser);
 //         vbox.add(&scan_button);
 //         vbox.add(&cancel_button);
 //         vbox.set_spacing(10);
-
 //         let window = gtk::Window::new(WindowType::Toplevel);
 //         window.set_title("Choose a directory to scan");
 //         window.add(&vbox);
 //         window.set_position(gtk::WindowPosition::Center);
 //         window.resize(300, 75);
 //         window.show_all();
-
 //         connect!(relm, scan_button, connect_clicked(_), ConfigMsg::StartScan);
 //         connect!(relm, cancel_button, connect_clicked(_), ConfigMsg::CancelScan);
 //         connect!(relm, file_chooser, connect_file_set(btn), ConfigMsg::GotPath(btn.get_filename()));
 //         connect!(relm, window, connect_delete_event(_, _), return (Some(ConfigMsg::Quit), Inhibit(false)));
-
 //         ConfigWindow {
 //             model,
 //             window,
@@ -166,3 +136,49 @@
 //         }
 //     }
 // }
+#[derive(Debug, Clone, Copy)]
+enum application_events {
+    DropdownSelected,
+    DirectorySelected,
+    RequestedScan,
+    RequestedCancel
+}
+struct analyzer_gui {
+    // model: ConfigModel,
+    // window: Window,
+    // directory_list: pick_list,
+    // // file_chooser: gtk::FileChooserButton,
+    // scan_button: Button,
+    // analyzer_win: Option<Component<analyzer::AnalyzerWindow>>,
+    // // cancel_sender: Option<Sender<()>>,
+    // cancel_button: gtk::Button
+ }
+
+ impl Application for analyzer_gui {
+     type Executor = executor::Default;
+     type Flags = ();
+     type Message = application_events;
+     type Theme = Theme;
+ 
+    // __x: () = unused variable with unspecified type
+    // in contrast to
+    // y: int
+    fn new(__flags: ()) -> (analyzer_gui, Command<application_events>) {
+       (analyzer_gui {}, Command::none())
+    }
+    fn view(&self) -> Element<application_events> {
+        container(app_context).height(Length::Fill)
+            .center_y().into()
+    }
+    fn title(&self) -> String {
+       String::from("Disk Analyzer")
+   }
+    fn update(&mut self, message: application_events) -> Command<Self::Message> {
+       match message {
+        application_events::DropdownSelected => { Command::none() },
+        application_events::DirectorySelected => { Command::none() },
+        application_events::RequestedScan => { Command::none() },
+        application_events::RequestedCancel => { Command::none() },
+       }
+    }
+ }
--- src/entry.rs
+++ src/entry.rs
@@ -5,56 +5,13 @@
 #![windows_subsystem = "windows"]
 #![feature(start)]
 
-use iced::{widget::{button, column, text}, Settings, Command, Application, Theme, Element};
-use iced::executor;
 // mod dir_walker;
 // mod analyzer;
-// mod config_window;
-
-struct Counter {
-   value: i32,
-}
-#[derive(Debug, Clone, Copy)]
-pub enum Message {
-   IncrementPressed,
-   DecrementPressed
-}
-
-impl Application for Counter {
-    type Executor = executor::Default;
-    type Flags = ();
-    type Message = Message;
-    type Theme = Theme;
-
-   // __x: () = unused variable with unspecified type
-   // in contrast to
-   // y: int
-   fn new(__flags: ()) -> (Counter, Command<Self::Message>) {
-      (Counter { value: 0 }, Command::none())
-      // Counter { value: 0 }
-   }
-   fn view(&self) -> Element<Message> {
-      column![
-         button("+").on_press(Message::IncrementPressed),
-         text(self.value).size(50),
-         button("-").on_press(Message::DecrementPressed)
-      ].into()
-   }
-   fn title(&self) -> String {
-      String::from("A cool application")
-  }
-   fn update(&mut self, message: Message) -> Command<Message> {
-      match message {
-         Message::IncrementPressed => { self.value += 1; Command::none() },
-         Message::DecrementPressed => { self.value -= 1; Command::none() },
-      }
-   }
-}
+mod application_gui;
 
 // #[start]
 // fn main(x: isize, y: *const *const u8) -> iced::Result {
 // fn init() -> iced::Result {
 fn main() -> iced::Result {
-   Counter::run(Settings::default())
-   // config_window::ConfigWindow::run(()).unwrap(); 
+   application_gui::Application::run(Settings::default())
 }
