e9b5f2128e55a032b01b619329f22159927e7481
Albert Snow <ajsnow2012@gmail.com>
2024-01-11 20:39:04 -0600

    working on test implmentation of iced

--- src/entry.rs
+++ src/entry.rs
@@ -5,10 +5,54 @@
 #![windows_subsystem = "windows"]
 #![feature(start)]
 
+use iced::{widget::{button, column, text, Column}, Settings, Command, Application, Theme};
+use iced::executor;
 mod dir_walker;
 mod analyzer;
 mod config_window;
+
+struct Counter {
+   value: i32,
+}
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+   IncrementPressed,
+   DecrementPressed
+}
+
+impl Application for Counter {
+    type Executor = executor::Default;
+    type Flags = ();
+    type Message = ();
+    type Theme = Theme;
+
+   // __x: () = unused variable with unspecified type
+   // in contrast to
+   // y: int
+   fn new(__flags: ()) -> (Counter, Command<Self::Message>) {
+      (Counter { value: 0 }, Command::none())
+      // Counter { value: 0 }
+   }
+   fn view(&self) -> Column<Message> {
+      column![
+         button("+").on_press(Message::IncrementPressed),
+         text(self.value).size(50),
+         button("-").on_press(Message::DecrementPressed)
+      ]
+   }
+   fn title(&self) -> String {
+      String::from("A cool application")
+  }
+   fn update(&mut self, message: Message) {
+      match message {
+         Message::IncrementPressed => { self.value += 1 },
+         Message::DecrementPressed => { self.value -= 1 },
+      }
+   }
+}
+
 #[start]
-fn init() {
-   config_window::ConfigWindow::run(()).unwrap(); 
+fn init() -> iced::Result {
+   Counter::run(Settings::default());
+   // config_window::ConfigWindow::run(()).unwrap(); 
 }
